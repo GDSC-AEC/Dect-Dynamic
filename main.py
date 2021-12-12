07291c71175e874d68e6a9888f54ce218b2f6779
Jiya <60106386+Jiya-Ull-Haq@users.noreply.github.com>
2021-12-12 22:37:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,19 @@ from fastapi import FastAPI
 import json
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 arr = []
+origins = ["*"]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/organizing")
 async def OCommittee():

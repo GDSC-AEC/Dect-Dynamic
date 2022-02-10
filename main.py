763b75031e2971634a5bb506270d6d07b43fe432
Jiya <60106386+Jiya-Ull-Haq@users.noreply.github.com>
2022-02-11 02:22:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -62,6 +62,20 @@ async def ICommittee():
     x = sheet.col_values(3)
     return x
 
+@app.get("/technical")
+async def TCommittee():
+    # --------------------------------------------------------------------------------------
+    # SHEET SETUP
+    scope = ["https://spreadsheets.google.com/feeds",
+             "https://www.googleapis.com/auth/spreadsheets",
+             "https://www.googleapis.com/auth/drive.file",
+             "https://www.googleapis.com/auth/drive"]
+    credentials = ServiceAccountCredentials.from_json_keyfile_name('static/key/api.json', scope)
+    client = gspread.authorize(credentials)
+    sheet = client.open_by_key("1jGMF0OBt8x20C8eLX9u6Qx8sADyy870kLyxE0HO_U2g").sheet1
+    # --------------------------------------------------------------------------------------
+    x = sheet.col_values(5)
+    return x
 
 @app.get("/national")
 async def NCommittee():
